e5da0b79d580602ba079fe840a46bbf7519d8c19
rlaxodnd98 <63450611+rlaxodnd98@users.noreply.github.com>
2023-01-06 00:17:55 +0900

    Create 2075_memory_overflow.py

---
+++ 2075_memory_overflow.py
@@ -0,0 +1,38 @@
+def find_biggest(buffer, N):
+    max_value = buffer[0]
+    max_index = 0
+    
+    for i in range(1, N):
+        if buffer[i] > max_value:
+            max_value = buffer[i]
+            max_index = i
+    
+    # 새로운 값으로 변경
+    return max_index
+    
+    
+def main():
+    N = int(input())
+    L = []
+    
+    # 각 행별 최대값의 index
+    index_list = [N-1 for _ in range(N)]
+    for i in range(N):
+        buffer = input().split()
+        for j in range(N):
+            buffer[j] = int(buffer[j])
+        L.append(buffer)
+    
+    # buffer변수는 가장 큰 N개의 변수를 가지고 있다.
+    
+    for x in range(N-1):
+        index = find_biggest(buffer, N)
+    
+        index_list[index] -= 1
+        buffer[index] = L[index_list[index]][index]
+        
+    index = find_biggest(buffer, N)
+    
+    print(buffer[index])
+    
+main()
