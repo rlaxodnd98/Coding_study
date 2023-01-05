eb21d33040301cf955a2906504a1a9a0f1e72440
rlaxodnd98 <63450611+rlaxodnd98@users.noreply.github.com>
2023-01-06 00:18:22 +0900

    Create 2075.py

---
+++ 2075.py
@@ -0,0 +1,19 @@
+N = int(input())
+L = []
+    
+    
+# 메모리 사용량 줄이기 위해, 절반씩만 사용
+# sorted함수를 사용해서 제일 큰것 N개만 추려냄
+for i in range(N // 2):
+    buffer = input().split()
+    for j in range(N):
+        L.append( int(buffer[j]) )
+    L = sorted(L, reverse=True)[0:N]
+    
+for i in range(N//2, N):
+    buffer = input().split()
+    for j in range(N):
+        L.append( int(buffer[j]) )
+    L = sorted(L, reverse=True)[0:N]
+    
+print(L[-1])
